854aafd57fbc46b56a8802205a5c53806c80342e
luowei <1184093898@qq.com>
2021-07-04 21:45:45 +0800

    写完CloudKit的教程代码

---
+++ 日记2.0/extension/Error+.swift
@@ -0,0 +1,83 @@
+//
+//  Error+.swift
+//  日记2.0
+//
+//  Created by 罗威 on 2021/7/4.
+//
+
+import Foundation
+import UIKit
+import os.log
+import CloudKit
+
+extension Error{
+    /// resolver是一个闭包，用于解决冲突，其返回的是无冲突的record。
+    ///
+    /// - Parameter : resolver: 一个闭包，接受两个参数分别为客户端上的record和服务器上的record。
+    /// - Returns: 由闭包resolver返回的，没有冲突的record。
+    func resolveConflict(with resolver:(CKRecord,CKRecord)->CKRecord?) ->CKRecord?{
+        guard let effectiveError = self as? CKError else{
+            os_log("resolveConflict called on an error that was not a CKError. The error was %{public}@",
+                   log: .default,
+                   type: .fault,
+                   String(describing: self))
+            return nil
+        }
+        
+        guard effectiveError.code == .serverRecordChanged else {
+            os_log("resolveConflict called on a CKError that was not a serverRecordChanged error. The error was %{public}@",
+                   log: .default,
+                   type: .fault,
+                   String(describing: effectiveError))
+            return nil
+        }
+        
+        //获取客户端上的record
+        guard let clientRecord = effectiveError.userInfo[CKRecordChangedErrorClientRecordKey] as? CKRecord else {
+            os_log("Failed to obtain client record from serverRecordChanged error. The error was %{public}@",
+                   log: .default,
+                   type: .fault,
+                   String(describing: effectiveError))
+            return nil
+        }
+        
+        //获取服务器上的record
+        guard let serverRecord = effectiveError.userInfo[CKRecordChangedErrorServerRecordKey] as? CKRecord else{
+            os_log("Failed to obtain server record from serverRecordChanged error. The error was %{public}@",
+                   log: .default,
+                   type: .fault,
+                   String(describing: effectiveError))
+            return nil
+        }
+        
+        //调用resolver闭包来处理二者的冲突
+        return resolver(clientRecord,serverRecord)
+    }
+    
+    /// 如果解析出服务器返回的错误是一个可恢复错误，则重新执行操作。
+    ///
+    /// - Parameters:
+    ///   - log: The logger to use for logging information about the error handling, uses the default one if not set
+    ///   - block: The block that will execute the operation later if it can be retried
+    /// - Returns: Whether or not it was possible to retry the operation
+    @discardableResult func retryCloudKitOperationIfPossible(_ log:OSLog? = nil,with block: @escaping () -> Void) -> Bool {
+        let effectiveLog:OSLog = log ?? .default
+        
+        //如果是可恢复的错误，错误的类型将会是CKError
+        guard let effectiveError = self as? CKError else {return false}
+        
+        guard let retryDelay:Double = effectiveError.retryAfterSeconds else {
+            os_log("此错误不是可恢复错误", log:effectiveLog,type:.error)
+            return false
+        }
+        
+        os_log("此错误是可恢复错误. 将会在 %{public}f 秒后重试", log: effectiveLog, type: .error, retryDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + retryDelay) {
+            block()//在delay后执行，即执行该block时此函数已经执行完毕，所以是逃逸闭包。
+        }
+        
+        return true
+    }
+}
+
--- 日记2.0/helper/cloudKitManager.swift
+++ 日记2.0/helper/cloudKitManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import CloudKit
+import UIKit
 
 class iCloudHelper{
     static let shared = iCloudHelper()
@@ -14,6 +15,18 @@ class iCloudHelper{
     var diaries:[CKRecord] = []
     let container = CKContainer.default()
     let database:CKDatabase = CKContainer.default().publicCloudDatabase
+    
+    //自定义的操作队列
+    private let cloudQueue = DispatchQueue(label: "SyncEngine.Cloud", qos: .userInitiated)
+    private lazy var cloudOperationQueue: OperationQueue = {
+        let q = OperationQueue()
+
+        q.underlyingQueue = cloudQueue
+        q.name = "SyncEngine.Cloud"
+        q.maxConcurrentOperationCount = 1
+
+        return q
+    }()
     
     init() {
         NotificationCenter.default.addObserver(self,
@@ -161,4 +174,83 @@ class iCloudHelper{
     @objc func userAccountChanged(){
         
     }
+    
+    ///创建订阅
+    func createSubscription(){
+        //注册显式通知
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { authorized, error in
+            guard error == nil, authorized else {
+                // not authorized...
+                return
+            }
+            
+            // subscription can be created now \o/
+        }
+        //（如果仅仅注册隐式通知，只需要registerForRemoteNotifications）
+        UIApplication.shared.registerForRemoteNotifications()
+        
+        
+        //创建一个订阅实例
+        let subscription = CKQuerySubscription(recordType: "DiaryContent",
+                                               predicate: NSPredicate(value: true),
+                                               subscriptionID: "update",
+                                               options: [.firesOnRecordCreation])
+        //配置通知
+        let info = CKSubscription.NotificationInfo()
+        info.alertLocalizationKey = "nearby_poi_alert"//用作alert的格式:
+        //例如“nearby_poi_alert” = “%@ has been added, check it out!”;
+        info.alertLocalizationArgs = ["title"]//从记录中取出该key的内容填充上面alert占位符的内容
+        info.soundName = "default"
+        info.desiredKeys = ["title"] //desiredKey是应该随通知一起发送的记录中的keys。
+        subscription.notificationInfo = info
+        
+        //保存订阅到云端
+        //*你想订阅哪个数据库，你就必须保存该数据库上
+        database.save(subscription) { (savedSubscription, error) in
+            guard let savedSubscription = savedSubscription , error == nil else{
+                //处理错误
+                return
+            }
+            //成功保存订阅
+            //(可能希望将subscription ID保存在User Default或其他位置)
+        }
+    }
+    
+    //获取云端的数据变化
+    func fetchRemoteChanges(){
+        // CKCurrentUserDefaultName is a "magic" constant provided by CloudKit which represents the currently logged in user
+        let customZoneID = CKRecordZone.ID(zoneName: "myDiaries", ownerName: CKCurrentUserDefaultName)
+        let operation = CKFetchRecordZoneChangesOperation()
+//        let token:CKServerChangeToken? = privateChangeToken//该属性还没有实现
+        let token:CKServerChangeToken? = nil
+        let config = CKFetchRecordZoneChangesOperation.ZoneConfiguration(previousServerChangeToken: token,
+                                                                         resultsLimit: nil,
+                                                                         desiredKeys: nil)
+        operation.configurationsByRecordZoneID = [customZoneID:config]
+        
+        operation.recordZoneIDs = [customZoneID]
+        operation.fetchAllChanges = true
+        
+        operation.recordZoneChangeTokensUpdatedBlock = {_,changeTolen,_ in
+            //存储changeToken以便在后续的提取中使用
+        }
+        
+        operation.recordZoneFetchCompletionBlock = { [weak self] _,_,_,_,error in
+            //如果有错误，处理错误
+        }
+        
+        operation.recordChangedBlock = {record in
+            //解析记录，然后存储到本地以便后续使用
+        }
+        
+        operation.recordWithIDWasDeletedBlock = {recordID,_ in
+            //删除recordID代表的本地实体
+        }
+        
+        operation.qualityOfService = .userInitiated
+        operation.database = database
+        
+        cloudOperationQueue.addOperation(operation)
+    }
+    
 }
