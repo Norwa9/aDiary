e073cc1c34a35bcbd36df09a91cd0f80855ebcc4
luowei3 <luowei3@yy.com>
2021-07-20 17:34:14 +0800

    创建LWTextView文件

---
+++ 日记2.0/today VC/LWtextView/LWTextView.swift
@@ -0,0 +1,20 @@
+//
+//  LWTextView.swift
+//  日记2.0
+//
+//  Created by yy on 2021/7/20.
+//
+
+import UIKit
+
+class LWTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
