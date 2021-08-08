b164dc3ba86ebb193d91afd13a00947e3c81a5b3
luowei <1184093898@qq.com>
2021-08-08 10:46:09 +0800

    iap

---
+++ 日记2.0/helper/IAPHelper/SKProduct+.swift
@@ -0,0 +1,20 @@
+//
+//  SKProduct+.swift
+//  日记2.0
+//
+//  Created by 罗威 on 2021/8/7.
+//
+
+import Foundation
+import StoreKit
+
+//MARK:-本地化价格
+extension SKProduct{
+    var regularPrice:String?{
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = self.priceLocale
+        return formatter.string(from: self.price)
+        //https://link.medium.com/wQrhCVUIwib
+    }
+}
