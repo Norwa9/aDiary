39516539a4c61cf71f889c499d721c2ac630a486
luowei3 <luowei3@yy.com>
2021-08-13 18:30:05 +0800

    widget空文件

---
+++ 日记2.0/helper/LWWidget/Data/roamData.swift
@@ -0,0 +1,8 @@
+//
+//  roamData.swift
+//  日记2.0
+//
+//  Created by yy on 2021/8/13.
+//
+
+import Foundation
---
+++ 日记2.0/helper/LWWidget/View/roamView.swift
@@ -0,0 +1,8 @@
+//
+//  roamView.swift
+//  日记2.0
+//
+//  Created by yy on 2021/8/13.
+//
+
+import Foundation
