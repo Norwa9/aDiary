7743243cb7350ac21e09d35bc1e78bba881d4e90
luowei <1184093898@qq.com>
2021-08-29 20:04:05 +0800

    图片添加删除选项

--- 日记2.0/today VC/ScableImageView/helper/LWPopManger.swift
+++ 日记2.0/today VC/ScableImageView/helper/LWPopManger.swift
@@ -82,6 +82,14 @@ class LWPopManager: NSObject {
                 view.delegate?.reloadScableImage(endView: view,shouldAddDoneView: true)
                 self.dismissPopMenu()
             }),
+            PopMenuDefaultAction(title: "删除",color: .red, didSelect: { action in
+                view.viewModel.getNewestLocation(attributedString: view.delegate!.attributedText) {
+                    let deleteRange = NSRange(location: view.viewModel.location, length: 1)
+                    view.delegate?.textStorage.replaceCharacters(in: deleteRange, with: " ")
+                    view.delegate?.textStorage.addAttribute(.paragraphStyle, value: leftParagraphStyle, range: deleteRange)
+                }
+                self.dismissPopMenu()
+            }),
         ]
         popManager.present(sourceView: view, on: nil, animated: true, completion: nil)
     }
